83d7dddd3ee101e2f2fd70d38a545485bc604f12
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-31 21:34:04 +0900

    apiproxy

---
+++ src/routes/apiproxy/+server.ts
@@ -0,0 +1,12 @@
+/*
+HTTP API
+"GET"|"POST"|"DELETE"|"PATCH",
+version: 10
+header:
+  X-Audit-Log-Reason
+  Authorization
+    https://discord.com/developers/docs/reference#authentication
+body: {}
+
+Gateway API
+*/
