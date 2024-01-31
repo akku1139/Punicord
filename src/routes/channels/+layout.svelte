172bac8c0fe9292a5bcca8bd6130c15eeebdbde8
-akku- <151517253+akku1139@users.noreply.github.com>
2024-01-31 16:59:25 +0900

    test

--- src/routes/channels/+layout.svelte
+++ src/routes/channels/+layout.svelte
@@ -1,1 +1,16 @@
+<script>
+import { readable } from "svelte/store";
+
+export const socketStore = readable({}, set => {
+    const socket = new WebSocket("wss://gateway.discord.gg");
+
+    socket.addEventListener("message", function (event) {
+      const data = JSON.parse(event.data);
+      set(data);
+    });
+
+    return () => { socket.close() };
+});
+</script>
+
 <slot />
